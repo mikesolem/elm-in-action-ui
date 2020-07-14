61692c1b380713c7a45b7d12e9440407afebde32
Mike <Michael.Solem@Resideo.com>
2020-07-14 14:49:12 -0400

    Minor tweak to make PhotoGroove look more like example from book.

--- PhotoGroove/src/PhotoGroove.elm
+++ PhotoGroove/src/PhotoGroove.elm
@@ -1,19 +1,19 @@
 module PhotoGroove exposing (main)
-        
-import Element exposing (..)
 
+import Element exposing (..)
+import Element.Font as Font
+import Element.Region as Region
 
 view model =
     layout [] <|
         column [ height fill, width fill ]
-            [ text "Photo Groove"
+            [ el [Font.size 32] (text "Photo Groove")
             , row [ height fill, width fill ]
                 [ image [ height fill ] {src="http://elm-in-action.com/1.jpeg", description="first"}
                 , image [ height fill ] {src="http://elm-in-action.com/2.jpeg", description="second"}
                 , image [ height fill ] {src="http://elm-in-action.com/3.jpeg", description="third"}  ]
             ]
-    
+
 
 main =
     view "no view yet"
-        
